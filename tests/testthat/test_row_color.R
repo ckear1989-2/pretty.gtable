7adf9dab7dbe0ca39e9c65236fbf31dc84135e84
ckear1989-2 <ckear1989@gmail.com>
2024-01-19 18:49:35 +0000

    rainbow table test

--- tests/testthat/test_row_color.R
+++ tests/testthat/test_row_color.R
@@ -14,3 +14,20 @@ test_that("row colorisation works", {
   expect_silent(pretty_gtable(mtcars, table.options, outf, truncate = 1:16))
   expect(difftime(Sys.time(), file.info(outf)$mtime, units = "secs") < 10, paste(outf, "not created in last 10 seconds."))
 })
+
+test_that("row colorisation works for one or several clors", {
+  table.options <- list(
+    rows = rownames(mtcars),
+    cols = colnames(mtcars),
+    width = 9,
+    height = 4,
+    rowcs = "red1"
+  )
+  outf <- "output/mtcars_16_row_color_all_red.pdf"
+  expect_silent(pretty_gtable(mtcars, table.options, outf, truncate = 1:16))
+  expect(difftime(Sys.time(), file.info(outf)$mtime, units = "secs") < 10, paste(outf, "not created in last 10 seconds."))
+  table.options$rowcs <- c("red", "orange", "yellow", "green", "blue", "purple", "violet")
+  outf <- "output/mtcars_16_row_color_rainbow.pdf"
+  expect_silent(pretty_gtable(mtcars, table.options, outf, truncate = 1:16))
+  expect(difftime(Sys.time(), file.info(outf)$mtime, units = "secs") < 10, paste(outf, "not created in last 10 seconds."))
+})
